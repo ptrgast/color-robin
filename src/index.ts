92867f1a88fab0608d28ced291cdaac5a0820966
Petros Gasteratos <ptrgast@users.noreply.github.com>
2024-03-16 18:50:58 +0200

    feat: Make thumbnail resolution configurable in analyzeImage()

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,20 @@ import { Thumbnail } from './library/Thumbnail';
 import { Histogram } from './library/Histogram';
 
 
-export function analyzeImage(image: HTMLImageElement, maxClasses: number = 2) {
+export function analyzeImage(image: HTMLImageElement, resolution: number = 20, maxClasses: number = 2) {
     const result = new Promise<Histogram>((resolve) => {
         if (image.complete) {
-            generateHistogram(image, resolve, maxClasses)();
+            generateHistogram(image, resolve, resolution, maxClasses)();
         } else {
-            image.addEventListener("load", generateHistogram(image, resolve,maxClasses));
+            image.addEventListener("load", generateHistogram(image, resolve, resolution, maxClasses));
         }
     });
     return result;
 }
 
-function generateHistogram(image: HTMLImageElement, resolve: any, maxClasses: number) {
+function generateHistogram(image: HTMLImageElement, resolve: any, resolution: number, maxClasses: number) {
     return () => {
-        const thumbnail = new Thumbnail(image);
+        const thumbnail = new Thumbnail(image, resolution);
         const histogram = new Histogram(thumbnail, maxClasses);
         resolve(histogram);
     };
